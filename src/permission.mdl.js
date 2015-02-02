415da9876094c6bf2dc208c84c8c60573f81fb81
flyskyko <sinsy200@gmail.com>
2015-02-02 17:53:29 +0900

    change a tab to space

--- src/permission.mdl.js
+++ src/permission.mdl.js
@@ -26,7 +26,7 @@
 
         if (permissions) {
           event.preventDefault();
-	  toState = angular.extend({'$$finishAuthorize': true}, toState);
+          toState = angular.extend({'$$finishAuthorize': true}, toState);
 
           Permission.authorize(permissions, toParams).then(function () {
             // If authorized, use call state.go without triggering the event.
